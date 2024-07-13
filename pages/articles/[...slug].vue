ce1b34d1cd92ac2db53fbbe83b9826155b58a3b5
Ivan Sitkin <alvinera@yandex.ru>
2024-07-13 17:04:05 +0200

    init

---
+++ pages/articles/[...slug].vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <ContentRenderer :value="article" />
+  </main>
+</template>
+
+
+<script setup>
+const route = useRoute();
+const { data: article } = await useAsyncData("article", () =>
+  queryContent(`/articles/${route.params.slug}`).findOne()
+);
+</script>
---
+++ pages/articles/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div v-for="article in articles">
+      <h3>{{ article.title }}</h3>
+      <a :href="article._path">Read More</a>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { data: articles } = await useAsyncData("articles", () => queryContent("/articles").find());
+</script>
