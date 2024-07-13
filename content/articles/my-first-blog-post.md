388babbd9a4b75677c207fba510df282bfffe0c5
Ivan Sitkin <alvinera@yandex.ru>
2024-07-13 19:34:34 +0200

    init

--- content/articles/my-first-blog-post.md
+++ content/articles/my-first-blog-post.md
@@ -1,8 +1,9 @@
 ---
-description: Welcome
-datetime: 2024-07-13 18:16
+datetime: "2024-07-13 18:16"
 ---
 
 # My first blog post
+
+Welcome
 
 Content
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  content: {
+    contentHead: true
+  },
   devtools: { enabled: true },
   modules: ['@nuxt/content', "@nuxt/image", "@nuxtjs/tailwindcss"],
   components: true,
