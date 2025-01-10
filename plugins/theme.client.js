ef525450a78cbf2e3c0623d5e177196c2ee74092
Ivan Sitkin <alvinera@yandex.ru>
2025-01-10 11:11:17 +0100

    fix: remove unused

--- plugins/theme.client.js
+++ plugins/theme.client.js
@@ -1,18 +1,16 @@
 export default defineNuxtPlugin((nuxtApp) => {
-  useHead({
-    script: [
-      {
-        children: `
+  if (import.meta.client) {
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.text = `
 (function () {
   const isDarkMode =
     localStorage.theme === "dark" ||
     (!("theme" in localStorage) &&
       window.matchMedia("(prefers-color-scheme: dark)").matches);
   document.documentElement.classList.toggle("dark", isDarkMode);
-})();`,
-        type: "text/javascript",
-        tagPriority: "critical",
-      },
-    ],
-  });
+})();`;
+    const head = document.head;
+    head.insertBefore(script, head.firstChild);
+  }
 });
